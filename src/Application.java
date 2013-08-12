6c68b56b831731058357b0df1bc9dc20ca22612c
Colin McEnearney <c.mcenearney@us.nature.com>
2013-08-11 23:06:47 -0400

    skeleton classes + TileBag init from config

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        /*
+        TODO: ask how many players
+        TODO: instantiate new Game?
+        */
+        Scanner input = new Scanner(System.in);
+        System.out.println("how many players?");
+        int num_players = input.nextInt();
+        //Game game = new Game(num_players);
+        //game.start();
 
     }
 }
